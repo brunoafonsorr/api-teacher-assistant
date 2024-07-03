42145f056871ed7aa26c594d43106aa4f60a89ff
brunoafonsorr <bruno.afonsorr.programador@gmail.com>
2024-07-03 14:00:12 -0300

    create teacher table

---
+++ src/database/migrations/00_create_teachers.ts
@@ -0,0 +1,14 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('teachers', table => {
+    table.increments('rm').primary();
+    table.string('name').notNullable();
+    table.string('avatar').notNullable();
+    table.string('password').notNullable();
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('teaches')
+}
