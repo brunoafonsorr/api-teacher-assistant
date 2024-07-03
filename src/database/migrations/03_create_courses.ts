ee4b5250bc376c0a6e51fdfad84d7f5f8e630183
brunoafonsorr <bruno.afonsorr.programador@gmail.com>
2024-07-03 18:58:23 -0300

    create courses table

--- src/database/migrations/03_create_courses.ts
+++ src/database/migrations/03_create_courses.ts
@@ -1,13 +1,13 @@
 import { Knex } from "knex";
 
-// !Description: table to record subjects, such as "mathematics", "marketing"
+// !Description: table to record courses, such as "administration", "HR"
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('subjects', table => {
+  return knex.schema.createTable('courses', table => {
     table.increments('id').primary();
     table.string('name').notNullable();
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('subjects')
+  return knex.schema.dropTable('courses')
 }
