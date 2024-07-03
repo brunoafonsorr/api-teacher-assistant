14e6bfad5fe39374aa5388d53db534e1c4004ae3
brunoafonsorr <bruno.afonsorr.programador@gmail.com>
2024-07-03 20:02:30 -0300

    create classes table

---
+++ src/database/migrations/07_create_classes.ts
@@ -0,0 +1,33 @@
+import { Knex } from "knex";
+
+// !Description: table to record classes, (teacher), (students), subject given in detail 
+// and whether there was support material, such as "slide", "work", "activities", "presentation"
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('classes', table => {
+    table.increments('id').primary();
+    table.string('description').notNullable();
+    table.string('content').notNullable();
+
+    table.integer('teacher_id')
+      .notNullable()
+      .references('rm')
+      .inTable('teachers')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+    
+    table.integer('subject_id')
+      .notNullable()
+      .references('id')
+      .inTable('subjects')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+    
+    table.timestamp('date')
+      .defaultTo(knex.raw('CURRENT_TIMESTAMP'))
+      .notNullable();
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('classes')
+}
