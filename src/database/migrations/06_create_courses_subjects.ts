ed450437b77165326d9681964317a4465f3adf05
brunoafonsorr <bruno.afonsorr.programador@gmail.com>
2024-07-03 19:56:12 -0300

    create courses_subjects table

---
+++ src/database/migrations/06_create_courses_subjects.ts
@@ -0,0 +1,27 @@
+import { Knex } from "knex";
+
+// !Description: table for list of courses and subjects
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('courses_subjects', table => {
+    table.increments('id').primary();
+    table.string('name').notNullable() // "marketing imobiliário"
+
+    table.integer('course_id')
+      .notNullable()
+      .references('id')
+      .inTable('courses')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+    
+    table.integer('subject_id')
+      .notNullable()
+      .references('id')
+      .inTable('subjects')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('courses_subjects')
+}
