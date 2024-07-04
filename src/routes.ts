9de727b204d9c6924740745a24d0d6804601844f
brunoafonsorr <bruno.afonsorr.programador@gmail.com>
2024-07-03 21:08:46 -0300

    create StudentsController and created its respective route

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,37 @@
 import express from 'express';
+import db from './database/connections';
+import StudentsController from './controllers/StudentsController';
 
 const routes = express.Router();
+const studentsController = new StudentsController();
+
+interface GradeData{
+  value: string,
+  description: string
+};
 
 routes.get('/prof', (req, res) => {
   
 })
-
 routes.post('/prof', (req, res) => {
   
 })
+
+routes.get('/students', studentsController.index)
+routes.post('/students', studentsController.create)
+
+// routes.post('/grades', async (req, res) => {
+//   const {
+//     value,
+//     description
+//   } = req.body;
+
+//   await db('grades').insert({
+//     value,
+//     description
+//   })
+
+//   return res.status(200).json({ message: 'success' })
+// })
 
 export default routes
